8aeeb03bd002685bf8df90c0be3ac19a1dd353b3
Abdullah <abbad9753@gmail.com>
2026-01-11 04:55:05 +0300

    svg

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 
 export default function LogoIcon(props: React.ComponentProps<'svg'>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 32 32"
-      {...props} 
-    >
-      <path
-        fill="currentColor"
-        d="M16 2L2 28h28L16 2zm0 4.5L25.5 26H6.5L16 6.5z" 
-      />
-    </svg>
+    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+ width="256.000000pt" height="256.000000pt" viewBox="0 0 256.000000 256.000000"
+ preserveAspectRatio="xMidYMid meet">
+<metadata>
+Created by potrace 1.16, written by Peter Selinger 2001-2019
+</metadata>
+<g transform="translate(0.000000,256.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">
+<path d="M1343 2033 c-44 -3 -76 -9 -82 -17 -7 -8 -76 -214 -155 -457 l-143
+-444 -29 -3 c-36 -4 -79 27 -88 62 -4 17 24 119 84 311 49 158 90 293 90 301
+0 24 -41 15 -68 -15 -40 -43 -298 -508 -316 -570 -37 -127 -4 -219 99 -274 39
+-22 47 -22 355 -17 549 9 579 8 596 -14 15 -21 18 -63 8 -91 -14 -36 -53 -43
+-299 -49 -267 -7 -271 -7 -284 56 l-7 33 -104 -3 c-58 -2 -109 -8 -115 -13
+-15 -15 -9 -105 10 -155 21 -55 84 -110 149 -129 56 -17 218 -19 486 -6 193 9
+232 21 308 98 147 148 130 379 -34 459 l-57 28 -274 -3 c-211 -2 -273 0 -273
+10 0 7 63 209 141 450 77 241 138 443 134 449 -3 5 -18 9 -33 8 -15 -1 -59 -4
+-99 -5z"/>
+</g>
+</svg>
+
   );
 }
