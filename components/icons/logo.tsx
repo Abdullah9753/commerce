54f23e97803139ed05a0afc6b89efc6e3d2c2ed6
Abdullah <abbad9753@gmail.com>
2026-01-11 04:47:12 +0300

    fix: replace image logo with svg to fix opengraph build

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,13 +1,16 @@
-import clsx from "clsx";
-import Image from "next/image";
+import React from 'react';
 
-export default function LogoIcon(props: { className?: string }) {
+export default function LogoIcon(props: React.ComponentProps<'svg'>) {
   return (
-    <Image
-      src="/favicon.ico"
-      alt={`${process.env.SITE_NAME} logo`}
-      fill
-      className={clsx("object-cover", props.className)}
-    />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      {...props} 
+    >
+      <path
+        fill="currentColor"
+        d="M16 2L2 28h28L16 2zm0 4.5L25.5 26H6.5L16 6.5z" 
+      />
+    </svg>
   );
 }
